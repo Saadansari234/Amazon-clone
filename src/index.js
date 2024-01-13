368c3e087e8a77a39fdb6ef119cb18f5c955e9af
saadansari234 <saadansari3212@gmail.com>
2024-01-14 00:24:08 +0530

    added all files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Tailvind from './tailwind css/Tailvind';
 // import App from './amazon/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-   <Tailvind />
-    {/* <App /> */}
+   {/* <Tailvind /> */}
+    <App />
   </>
 );
 
